9af1893898001c132329bac918d4c93c1faee2dd
Umme-Hani-Khanam <khanamummehani0@gmail.com>
2026-01-31 08:13:50 +0530

    final commit

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config()
 const app=express();
 app.use(express.json());
-const port=process.env.PORT
+const port=process.env.PORT || 8000
 app.listen(port,()=>{
-    console.log('Server running at PORT ',port);
+    console.log(`Server running at PORT ${port}`);
 
 })
